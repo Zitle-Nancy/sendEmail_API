3acddd64ac16e7c6d5f4dad86bb6e4a24c3c82c0
Nancy <nancy@urbvan.com>
2020-04-10 23:47:48 -0500

    fix en la funcion de sendEmail

--- sendEmail.js
+++ sendEmail.js
@@ -1,36 +1,28 @@
 const nodemailer = require('nodemailer');
 
-var transporter = nodemailer.createTransport({
-	service: 'gmail',
-	auth: {
-		user: 'ancizj393@gmail.com',
-		pass: 'Noviembre07$'
-	}
- });
+const sendEmail = (address,template) => {
+	const transporter = nodemailer.createTransport({
+		service: 'gmail',
+		auth: {
+			user: 'ancizj393@gmail.com',
+			pass: 'Noviembre07$'
+		}
+	});
 
- let dataEmail = {
-	 address:'',
-	 template:''
+	const mailOptions = {
+		from: 'ancizj393@gmail.com', // sender address
+		to: `${address}`, // list of receivers
+		subject: 'prueba', // Subject line
+		html:  `${template}`// plain text body
+	};
+
+	transporter.sendMail(mailOptions, function (err, info) {
+		if(err)
+			console.log(err, 'error')
+		else
+			console.log(info, 'información');
+	});
  }
 
- const sendEmail = (address,template) => {
-	console.log(address, template)
-	dataEmail.address = address;
-	dataEmail.template = template;
- }
- console.log(dataEmail,'datyaEmail')
- const mailOptions = {
-  from: 'ancizj393@gmail.com', // sender address
-  to: `${dataEmail.address}`, // list of receivers
-  subject: 'prueba', // Subject line
-  html:  `${dataEmail.template}`// plain text body
-};
-
-transporter.sendMail(mailOptions, function (err, info) {
-	if(err)
-		console.log(err, 'error')
-	else
-		console.log(info, 'información');
-});
 
 module.exports = sendEmail;
